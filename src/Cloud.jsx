39a27617762847d8d58a06345acf62537d7d6897
Prototype <119293309+Four1err@users.noreply.github.com>
2023-11-03 19:10:14 +0800

    Update Cloud.jsx

--- src/Cloud.jsx
+++ src/Cloud.jsx
@@ -11,7 +11,7 @@ const Cloud = () => {
 			{new Array(num).fill(0).map((item, index) => (
 				<group
 					key={index}
-					position={[Math.random() * 20 - 10, Math.random() * 7 + 7, Math.random() * 20 - 10]}
+					position={[Math.random() * 20 - 10, Math.random() * 10 + 10, Math.random() * 20 - 10]}
 					rotation-y={Math.random() * Math.PI * 2}
 				>
 					<Instance position={[-1.85, Math.random() * 0.3, 0]} scale={1.2} />
